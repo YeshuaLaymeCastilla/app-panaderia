5184ea5e39fed22ac45ce5c869db1d8a69107927
YeshuaLaymeCastilla <yeshualayme567@gmail.com>
2026-02-19 13:54:42 -0500

    No comentarios

--- android/app/src/main/java/com/kamay/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.kamay;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.kamay',
+  appId: 'com.yeshua.kamay',
   appName: 'Kamay',
   webDir: 'dist'
 };
